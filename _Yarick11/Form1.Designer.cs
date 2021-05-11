2a0065e549632ed54f692257ee2bd5f545d39364
RGN <vnifedow@bk.ru>
2021-05-11 19:43:22 +0500

    Готово

--- _Yarick11/Form1.Designer.cs
+++ _Yarick11/Form1.Designer.cs
@@ -29,15 +29,26 @@ namespace _Yarick11
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.buttonStart = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(713, 415);
+            this.buttonStart.Location = new System.Drawing.Point(555, 413);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.Size = new System.Drawing.Size(520, 23);
             this.buttonStart.TabIndex = 0;
             this.buttonStart.Text = "Старт";
             this.buttonStart.UseVisualStyleBackColor = true;
@@ -46,21 +57,98 @@ namespace _Yarick11
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(526, 12);
+            this.listBox1.Location = new System.Drawing.Point(906, 23);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 394);
+            this.listBox1.Size = new System.Drawing.Size(169, 381);
             this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(730, 23);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(169, 381);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(903, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Равномерный";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(727, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Нормальный";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(555, 23);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(169, 381);
+            this.listBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(552, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Конкатенированный";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 413);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Нажми на кнопку";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(15, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(531, 392);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1087, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.buttonStart);
             this.Name = "Form1";
             this.Text = "ТРПО2";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +156,13 @@ namespace _Yarick11
 
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- _Yarick11/Form1.cs
+++ _Yarick11/Form1.cs
@@ -19,24 +19,74 @@ namespace _Yarick11
 
         List<double> Normal = new List<double>();
         List<double> Equal = new List<double>();
+        Random random = new Random();
 
-        double GetEqualValue()
+        double GetEqualValue() => (double)random.Next(0, 10);
+        double GetNormalValue(int mx, int dx) => (double)(1 / Math.Sqrt(dx) * Math.Sqrt(2 * Math.PI)) * Math.Exp(-1 * (Math.Pow(random.Next(0, 50) - mx, 2)) / 2 * dx);
+
+        List<double> ConcateList()
         {
-            Random random = new Random();
-            int a = random.Next(-10, 20);
-            return a < 0 || a > 10 ? 0 : 0.1;
+            List<double> Temp = new List<double>();
+            for(int i = 0; i < 50; i++)
+            {
+                Temp.Add(Normal[i]);
+                Temp.Add(Equal[i]);
+            }
+            Temp.Sort();
+            return Temp;
+        }
+
+        void Calculate()
+        {
+            double Average = 0;
+            double Dispersion = 0;
+            foreach (var i in ConcateList())
+            {
+                Average += i;
+            }
+            foreach (var i in ConcateList())
+            {
+                Dispersion += Math.Pow(i - (Average / ConcateList().Count), 2);
+            }
+            Dispersion /= ConcateList().Count - 1;
+            label4.Text = $"Среднее значение: {Average / ConcateList().Count} Дисперсия: {Dispersion}";
+        }
+        private void ChartSet()
+        {
+            label4.Text += "\n";
+            int i = 0; double sum = 0;
+            foreach (double d in ConcateList())
+            {
+                if (i % 10 == 0)
+                {
+                    label4.Text += $"Столб.{i / 10 + 1}: {sum} ";
+                    chart1.Series[0].Points.Add(sum);
+                    sum = 0;
+                }
+                else
+                {
+                    sum += d;
+                }
+                i++;
+            }
         }
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
+            buttonStart.Visible = false;
             for(int i = 0; i < 50; i++)
             {
                 Equal.Add(GetEqualValue());
+                Normal.Add(GetNormalValue(5, 4));
+                listBox1.Items.Add(Equal[i]);
+                listBox2.Items.Add(Normal[i]);
             }
-            foreach(double i in Equal)
+            foreach(double i in ConcateList())
             {
-                listBox1.Items.Add(i);
+                listBox3.Items.Add(i);
             }
+            Calculate();
+            ChartSet();
         }
     }
 }
